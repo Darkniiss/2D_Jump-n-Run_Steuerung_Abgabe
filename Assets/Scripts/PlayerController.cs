0a8eec7c5e973efcf266f216b59d3bdba068fa49
Finn Leo <finn.elias.leo@gmail.com>
2023-07-20 15:48:06 +0200

    Added Jump, Double Jump & Wall Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 10;
+    [SerializeField] private float jumpForce = 3;
+    [SerializeField] private bool canDoubleJump;
+    [SerializeField] private bool canWallJump;
 
     private Rigidbody2D playerRb;
     private SpriteRenderer playerSprite;
     private Vector2 moveVec;
+    private bool isGrounded;
+    private bool isAtWall;
+    private bool usedDoubleJump;
 
     void Start()
     {
@@ -20,6 +26,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+
         playerRb.velocity = new Vector2(moveVec.x * Time.deltaTime * moveSpeed, playerRb.velocity.y);
     }
 
@@ -35,6 +42,53 @@ public class PlayerController : MonoBehaviour
         else if (moveVec.x < 0)
         {
             playerSprite.flipX = false;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+        else if (!isGrounded && canDoubleJump && !usedDoubleJump && context.performed)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+            usedDoubleJump = true;
+        }
+    }
+
+    public void WallJump(InputAction.CallbackContext context)
+    {
+        if(isAtWall && canWallJump && context.performed)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            usedDoubleJump = false;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            isAtWall = true;
+            usedDoubleJump = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            isAtWall = false;
         }
     }
 
